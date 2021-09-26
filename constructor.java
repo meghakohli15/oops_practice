1b512eb798d93ee48d23e3295fd101f04b10c632
megha kohli <69910808+meghakohli15@users.noreply.github.com>
2021-09-26 15:13:14 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,16 @@
+//default constructor
+class Bike
+{
+  Bike1()
+  {
+    System.ou.println("riding the bike");
+  }
+}
+pubic class Mian
+{
+  public static void main(String[] args)
+  {
+    Bike1 b=new Bike1();
+  }
+}
+                         
