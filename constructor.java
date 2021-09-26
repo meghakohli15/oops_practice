0771ed39e3030941d588dbc04e60ac28d3a3451f
megha kohli <69910808+meghakohli15@users.noreply.github.com>
2021-09-26 15:23:56 +0530

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -14,3 +14,29 @@ pubic class Mian
   }
 }
                          
+//parameteried constructor
+class Student3
+{
+ int id;
+ String name;
+ Student3(int i,String n)
+{
+    id=i;
+    name=n;
+} 
+ void display()
+{
+    System.out.println(id+" "+name);
+    
+}
+}
+public class Main
+{
+public static void main(String args[])
+{
+Student3 s1=new Student3(4,"ko");
+Student3 s2=new Student3(6,"jo");
+s1.display();
+s2.display();
+}
+}
