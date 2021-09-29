e4a23056515ff743f212cadb46982e5941d843ae
megha kohli <69910808+meghakohli15@users.noreply.github.com>
2021-09-29 16:36:07 +0530

    Update exception.java

--- exception.java
+++ exception.java
@@ -37,3 +37,120 @@ public class Main {
     System.out.println("File saved successfully");  
     }  
 }  
+//multi-catch block
+public class MultipleCatchBlock1 {  
+  
+    public static void main(String[] args) {  
+          
+           try{    
+                int a[]=new int[5];    
+                a[5]=30/0;    
+               }    
+               catch(ArithmeticException e)  
+                  {  
+                   System.out.println("Arithmetic Exception occurs");  
+                  }    
+               catch(ArrayIndexOutOfBoundsException e)  
+                  {  
+                   System.out.println("ArrayIndexOutOfBounds Exception occurs");  
+                  }    
+               catch(Exception e)  
+                  {  
+                   System.out.println("Parent Exception occurs");  
+                  }             
+               System.out.println("rest of the code");    
+    }  
+}  
+?? at 1st exception caught still added other exceptions so compile time error
+class MultipleCatchBlock5{    
+  public static void main(String args[]){    
+   try{    
+    int a[]=new int[5];    
+    a[5]=30/0;    
+   }    
+   catch(Exception e){System.out.println("common task completed");}    
+   catch(ArithmeticException e){System.out.println("task1 is completed");}    
+   catch(ArrayIndexOutOfBoundsException e){System.out.println("task 2 completed");}    
+   System.out.println("rest of the code...");    
+ }    
+}   
+//nested try block
+public class Main{    
+ public static void main(String args[]){   
+ //outer try block   
+  try{    
+  //inner try block 1  
+    try{    
+     System.out.println("going to divide by 0");    
+     int b =39/0;    
+   }  
+    //catch block of inner try block 1  
+    catch(ArithmeticException e)  
+    {  
+      System.out.println(e);  
+    }    
+       
+    
+    //inner try block 2  
+    try{    
+    int a[]=new int[5];    
+  
+    //assigning the value out of array bounds  
+     a[5]=4;    
+     }  
+  
+    //catch block of inner try block 2  
+    catch(ArrayIndexOutOfBoundsException e)  
+    {  
+       System.out.println(e);  
+    }    
+  
+      
+    System.out.println("other statement");    
+  }  
+  //catch block of outer try block  
+  catch(Exception e)  
+  {  
+    System.out.println("handled the exception (outer catch)");  
+  }    
+    
+  System.out.println("normal flow..");    
+ }    
+}  
+//throwpublic class Main{  
+    //defining a method  
+    public static void checkNum(int num) {  
+        if (num < 1) {  
+            throw new ArithmeticException("\nNumber is negative, cannot calculate square");  
+        }  
+        else {  
+            System.out.println("Square of " + num + " is " + (num*num));  
+        }  
+    }  
+    //main method  
+    public static void main(String[] args) {  
+            Main obj = new Main();  
+            obj.checkNum(-3);  
+            System.out.println("Rest of the code..");  
+    }  
+}  
+throws
+  public class Main{  
+    //defining a method  
+    public static int divideNum(int m, int n) throws ArithmeticException {  
+        int div = m / n;  
+        return div;  
+    }  
+    //main method  
+    public static void main(String[] args) {  
+        Main obj = new Main();  
+        try {  
+            System.out.println(obj.divideNum(45, 0));  
+        }  
+        catch (ArithmeticException e){  
+            System.out.println("\nNumber cannot be divided by 0");  
+        }  
+          
+        System.out.println("Rest of the code..");  
+    }  
+}  
